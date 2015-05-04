bc978a96928b71e0c06dd85ea49c5c16835d38cb
RKoitsalu <raimond.koitsalu@gmail.com>
2015-05-04 15:24:22 +0300

    how to retrieve statements with mongo

---
+++ retrievingStatementsMongo.php
@@ -0,0 +1,29 @@
+<?php
+
+$connection = new MongoClient();
+$db = $connection->selectDB('learninglockerstable'); //Database name
+
+$collection = $db->statements; // Choose the statements collection
+
+$cursor = $collection->find();  // All statements in "learninglockerstable" database
+// http://php.net/manual/en/mongocollection.find.php
+
+
+foreach($cursor as $document){
+  //error_log(print_r($document, true));
+}
+
+
+$query = array(
+  'lrs._id' => '5448b8d7416fef053fd63af1', // Might want to add this to every query, if you have multiple LRS's in one learninglocker installation.
+  'statement.object.definition.type' => 'http://adlnet.gov/expapi/activities/course' // Retrieve all statements that have object->definition->type set as 'http://adlnet.gov/expapi/activities/course',
+);
+
+
+$cursor = $collection->find($query);
+foreach($cursor as $document){
+  error_log(print_r($document, true));
+}
+
+
+?>
